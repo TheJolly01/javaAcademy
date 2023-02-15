c4168e73046a2b6b01f62b1e2db1dda0e06112f0
Jolly <delrioantonio01@gmail.com>
2023-02-15 12:04:52 +0100

    Add files via upload

---
+++ ScriptJava/datipersonali.java
@@ -0,0 +1,16 @@
+public class datipersonali {
+    public static void main(String[] args){
+        String nome = "Antonio";
+        String cognome = "Delrio";
+        String anni = "21";
+        System.out.println("Stampa1");
+        System.out.println(nome +" "+ cognome +" "+ anni);
+
+        System.out.println("-------------------------");
+
+        String dati = nome + " " + cognome + " " + anni;
+        System.out.println("Stampa2");
+        System.out.println(dati);
+
+    }
+}
