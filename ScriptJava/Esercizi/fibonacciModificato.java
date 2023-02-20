10b9b3ecccd4651594a9e7ebac0c5d2690944638
Jolly <delrioantonio01@gmail.com>
2023-02-20 12:28:32 +0100

    Create fibonacciModificato.java
    Creata copia di fibonacci.java dove vengono stampati a terminale solamente i numeri pari

---
+++ ScriptJava/Esercizi/fibonacciModificato.java
@@ -0,0 +1,28 @@
+public class fibonacciModificato {
+    
+    public static void main(String[] args){
+        int numero1 = 0;
+        int numero2 = 1;
+        int numero = 0;
+        int nVolte = 20;
+
+        for (int i=0; i<nVolte; i++){
+
+            numero= numero1 + numero2;
+            numero1 = numero2;
+            numero2 = numero;
+            if(numero%2 == 0){
+                System.out.println(numero);
+            }
+
+            
+
+        }
+
+
+
+
+
+    }
+
+}
