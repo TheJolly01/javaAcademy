dc8a3276989897f83918b057b82181dbc6fe0cd1
Jolly <adelrio.dev@outlook.com>
2023-02-20 11:22:11 +0100

    Delete ScriptJava directory
    Delete directory to reupload new complete directory

--- ScriptJava/condizioni.java
+++
@@ -1,9 +0,0 @@
-public class condizioni {
-    public static void main(String[] args){
-        int x = 10;
-        int y = 9;
-
-        System.out.println(x>y);    //restituirà valore true
-        System.out.println(x<y);    //restituirà valore false
-    }
-}
