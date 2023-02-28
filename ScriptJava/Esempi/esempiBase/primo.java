65409048060a5961555acd45f24465d98531b550
Jolly <adelrio.dev@outlook.com>
2023-02-28 11:30:50 +0100

    Delete ScriptJava directory

--- ScriptJava/Esempi/esempiBase/primo.java
+++
@@ -1,6 +0,0 @@
-public class primo {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // stampa la scritta hello world
-    }
-}
--- ScriptJava/Esempi/esempiCondizionieLoop/If/condizioni.java
+++
@@ -1,9 +0,0 @@
-public class condizioni {
-    public static void main(String[] args) {
-        int x = 10; // dichiarazione e inizializzazione variabile x
-        int y = 9; // dichniarazione e inizializzazione variabile y
-
-        System.out.println(x > y); // restituirà valore true
-        System.out.println(x < y); // restituirà valore false
-    }
-}
